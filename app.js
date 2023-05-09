b8b0b0e36626b9a1818bdf273d4e1be27a2a971e
aahmadfaraz <afaraz2647@gmail.com>
2023-05-10 03:39:04 +0500

    updated

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/blog", blogRouter);
 
+// TODO: move user,pass & db to .env
 mongoose
   .connect(
     "mongodb+srv://admin:hOqBPxcX83Q6ZzVH@cluster0.yxacwnd.mongodb.net/Blog?retryWrites=true&w=majority"
@@ -17,5 +18,3 @@ mongoose
   .then(() => app.listen(5000))
   .then(() => console.log("Conntected to Database.."))
   .catch((err) => console.log(err));
-
-//hOqBPxcX83Q6ZzVH
